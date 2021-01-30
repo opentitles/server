6f9a240d2c4655ab4a31471ca679776c56d6d40f
Floris de Bijl <18357629+Fdebijl@users.noreply.github.com>
2021-01-30 20:16:09 +0100

    feat: add basic telemetry (#120)
    * feat: add basic telemetry
    
    * ci: add basic check suite
    
    * feat: add auth

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import moment from 'moment';
 import express from 'express';
+import prometheus from 'express-prometheus-middleware';
 import fs from 'fs';
 import cors from 'cors';
 import { MongoClient, Db } from 'mongodb';
@@ -25,6 +26,14 @@ if (process.env.DSN) {
       new Tracing.Integrations.Express({ app })
     ]
   });
+}
+
+if (CONFIG.EXPECTED_TELEMETRY_AUTH) {
+  app.use(prometheus({
+    metricsPath: `/v${CONFIG.REV}/metrics`,
+    collectDefaultMetrics: true,
+    authenticate: req => req.headers.authorization === `Basic ${CONFIG.EXPECTED_TELEMETRY_AUTH}`
+  }))
 }
 
 if (!fs.existsSync('media.json')) {
